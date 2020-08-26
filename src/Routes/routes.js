421d0b41960a5d84c95b4b2fdf60d5ceeef98a31
leonardo.perello <leo_16.04.96@hotmail.com>
2020-08-26 12:18:24 -0300

    arreglo bugs, auth no anda aun

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -3,15 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import PrivateRoute from '../services/auth.service';
 
-import Login from '../components/Login/Login';
-import Dashboard from '../components/Dashboard/Dashboard';
-//import Logout from './components/Logout';
+import Login from '../components/Login/Login';//import Logout from './components/Logout';
 
 const Routes = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={Login} />
-            <PrivateRoute path="/admin" component={Dashboard} />
             {/* <Route exact path="/logout" component={Logout} /> */}
         </Switch>
     </Router>
