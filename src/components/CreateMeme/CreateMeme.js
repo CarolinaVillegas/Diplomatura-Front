2817a261da27c86dc023317ee1cd1f3228f31813
leonardo.perello <leo_16.04.96@hotmail.com>
2020-09-02 11:12:44 -0300

    agrego campo de categoria al crear meme

--- src/components/CreateMeme/CreateMeme.js
+++ src/components/CreateMeme/CreateMeme.js
@@ -7,6 +7,7 @@ import {
   FormControl,
   Card,
   Modal,
+  Label,
 } from "react-bootstrap";
 //import Upload from "./Upload.js";
 //import GoogleLogin from "react-google-login";
@@ -20,7 +21,7 @@ export default function CreateMeme({}) {
   const [category, setCategory] = useState("");
   const [show, setShow] = useState(false);
   const [badLogin, setBadLogin] = useState(false);
- // const forceUpdate = React.useCallback(() => updateState({}), []);
+  // const forceUpdate = React.useCallback(() => updateState({}), []);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -32,9 +33,9 @@ export default function CreateMeme({}) {
     const fileField = document.querySelector("input[type='file']");
 
     formData.append("title", title);
-    formData.append("category", "anime");
+    formData.append("description", description);
+    formData.append("category", category);
     formData.append("img_source", fileField.files[0]);
-    
 
     fetch("/memes", {
       method: "POST",
@@ -44,7 +45,6 @@ export default function CreateMeme({}) {
       .then((data) => data.json())
       .then((response) => console.log(response))
       .catch((e) => console.log("Error: ", e));
-
   }
 
   return (
@@ -74,23 +74,30 @@ export default function CreateMeme({}) {
                 onChange={(e) => setDescription(e.target.value)}
               />
             </FormGroup>
-
-            <Card.Title>Category</Card.Title>
+            <Form.Group controlId="category">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                as="select"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option>general</option>
+                <option>random</option>
+                <option>política</option>
+                <option>deportes</option>
+                <option>animales</option>
+                <option>gaming</option>
+                <option>anime</option>
+              </Form.Control>
+            </Form.Group>
             <Card.Title>Imagen</Card.Title>
             <Form.File id="image-file" label="Seleccionar imagen" custom />
 
             <Modal.Footer>
-              <Button
-                block
-                onClick={handleSave}
-                type="submit"
-              >
+              <Button block onClick={handleSave} type="submit">
                 Upload
               </Button>
-              <Button
-                onClick={handleClose}
-                type="close"
-              >
+              <Button onClick={handleClose} type="close">
                 Publish
               </Button>
             </Modal.Footer>
