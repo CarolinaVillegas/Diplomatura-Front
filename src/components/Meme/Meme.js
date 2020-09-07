258770042ef8cff7372422c2ab08cce29ab32cf1
leonardo.perello <leo_16.04.96@hotmail.com>
2020-09-07 19:35:41 -0300

    cambio en el font-family de todo

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,24 +1,30 @@
-import React from "react";
-import { Col, Card, Button } from "react-bootstrap";
-import "./Meme.css";
-import {IoIosText, IoIosThumbsDown, IoIosThumbsUp } from "react-icons/io";
+import React from 'react';
+import { Col, Card, Button } from 'react-bootstrap';
+import './Meme.css';
+import { IoIosText, IoIosThumbsDown, IoIosThumbsUp } from 'react-icons/io';
 
 export default function Meme({ userData, addVoteHandler, removeVoteHandler }) {
   const data = userData.map((meme, i) => (
     <Col sm={6} md={8} key={i}>
-      <Card style={{ width: "35rem" }} className="cardPrimary">
+      <Card style={{ width: '35rem' }} className="cardPrimary">
         <Card.Title className="cardTitle">{meme.title}</Card.Title>
         <Card.Subtitle className="cardSubtitle">{meme.category}</Card.Subtitle>
-        <Card.Img src={meme.img_source} alt={meme.title} loading="lazy" />
-        <Card.Body>
-         Votes: {meme.points} <br />
-         Comments: {meme.comment_counter}
+        <Card.Img
+          className="cardIMG"
+          src={meme.img_source}
+          alt={meme.title}
+          loading="lazy"
+        />
+        <Card.Body className="cardBody">
+          Votes: {meme.points} <br />
+          Comments: {meme.comment_counter}
           <Card.Footer>
             <Button
               variant="success"
               className="botonesLike"
               onClick={() => addVoteHandler(meme)}
-            ><IoIosThumbsUp className="dedoLike"/>
+            >
+              <IoIosThumbsUp className="dedoLike" />
               Like
             </Button>
             <Button
@@ -26,11 +32,16 @@ export default function Meme({ userData, addVoteHandler, removeVoteHandler }) {
               className="botonesLike"
               onClick={() => removeVoteHandler(meme)}
             >
-             <IoIosThumbsDown className="dedoDislike"/> Dislike
+              <IoIosThumbsDown className="dedoDislike" /> Dislike
             </Button>
-            <Button className="buttonComment" /*cambiar a una ruta que lleve a ese meme individual y al comentario hay que pasar el meme y el usuario que va a comentar*/ meme = {meme} user = {userData}
+            <Button
+              className="buttonComment"
+              /*cambiar a una ruta que lleve a ese meme individual y al comentario hay que pasar el meme y el usuario que va a comentar*/ meme={
+                meme
+              }
+              user={userData}
             >
-            <IoIosText className="commentGlobe"/>   Comment
+              <IoIosText className="commentGlobe" /> Comment
             </Button>
           </Card.Footer>
         </Card.Body>
